4760d3eb18cebab2a3a5f89ee5680bd53bf4ddbd
Rohit <rrjadhav66@gmail.com>
2023-06-15 23:03:48 +0530

    Added Forms

--- src/app/forms/visual-deck-report-modal/visual-deck-report-modal.component.ts
+++ src/app/forms/visual-deck-report-modal/visual-deck-report-modal.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./visual-deck-report-modal.component.scss']
 })
 export class VisualDeckReportModalComponent {
-
+  exteriorElementsOptions = [
+    { value: 'option1', label: 'Option 1' },
+    { value: 'option2', label: 'Option 2' },
+    { value: 'option3', label: 'Option 3' }
+    // Add more options as needed
+  ];
 }
