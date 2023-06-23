0de5804c63367bc850d2b49ab143680a6685d26f
Rohit <rrjadhav66@gmail.com>
2023-06-24 05:23:42 +0530

    API Integrated

---
+++ src/app/common/models/inspection-report.ts
@@ -0,0 +1,20 @@
+export interface InspectionReport {
+  _id: string;
+  additionalconsiderations: string;
+  awe: string;
+  conditionalassessment: string;
+  createdat: string;
+  createdby: string;
+  editedat: string;
+  eee: string;
+  exteriorelements: string[];
+  furtherinvasivereviewrequired: boolean;
+  images: string[];
+  lasteditedby: string | null;
+  lbc: string;
+  name: string;
+  parentid: string;
+  visualreview: string;
+  visualsignsofleak: boolean;
+  waterproofingelements: string[];
+}
