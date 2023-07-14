012eafd3bf01bf91d6d34ccd89892c68485d23f6
umeshpalav <umeshpalav96@gmail.com>
2023-07-15 02:23:08 +0530

    Upload Image issue fixed

--- src/app/service/image-to-url-converter.service.ts
+++ src/app/service/image-to-url-converter.service.ts
@@ -1,29 +1,46 @@
 import { Injectable } from '@angular/core';
 import {HttpsRequestService} from "./https-request.service";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageToUrlConverterService {
 
-  constructor(private httpsRequestService:HttpsRequestService) {
+  constructor(private httpsRequestService:HttpsRequestService,private http: HttpClient) {
 
   }
 
-    public convertImageToUrl(data:any): string {
+     public async convertImageToUrl(data:any): Promise<string> {
+    //console.log(data);
+    const formData = new FormData();
+    formData.append('picture',data.picture, data.picture.name);
+    formData.append('containerName', 'inor');
+    formData.append('uploader', 'deck');
+    formData.append('entityName', 'Inorbit Mall');
+    let url = 'https://deckinspectors-dev.azurewebsites.net/api/image/upload';
+    //let url = 'https://localhost:3000/api/image/upload';
+    let s3URL='';
 
-      let url = 'https://deckinspectors-dev.azurewebsites.net/api/image/upload';
-      let s3URL='';
+    const headers = new HttpHeaders();
+    headers.append('Content-Type', 'multipart/form-data'); // Set the correct Content-Type
 
-      this.httpsRequestService.postHttpData(url, data).subscribe(
-        (response: any) => {
-          console.log(response);
-          s3URL = response.data.url;
-        },
-        error => {
-          console.log(error)
-        }
-      );
+  
+
+    try {
+      const response = await this.http.post(url, formData, { headers }).toPromise();
+      if (response) {
+        console.log(response);
+      } else {
+        console.log(response);
+      }
+    } catch (error) {
+      // Handle error
+      console.error('Error uploading image:', error);
+      throw error;
+    }
       return s3URL;
     }
 
