8b2faa96d78b93ca208d03046818a38d44d4b6f8
Rohit <rrjadhav66@gmail.com>
2023-07-23 03:36:38 +0530

    Updated Following Functionalities 1. Added Highlight at section level 2. CSS fix on section page 3. Report Download Partial HTML Template

--- src/app/components/project/location-list/location-details/section-list/section-list.component.ts
+++ src/app/components/project/location-list/location-details/section-list/section-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
 import {
   VisualDeckReportModalComponent
