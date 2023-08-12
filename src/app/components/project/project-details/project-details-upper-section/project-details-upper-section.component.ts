4d3c807dc6d4faae2a19f8a0f0c27e233145180c
Rohit <rrjadhav66@gmail.com>
2023-08-12 20:07:46 +0530

    Cursor pointer added on missing icons

--- src/app/components/project/project-details/project-details-upper-section/project-details-upper-section.component.ts
+++ src/app/components/project/project-details/project-details-upper-section/project-details-upper-section.component.ts
@@ -66,7 +66,6 @@ export class ProjectDetailsUpperSectionComponent implements OnInit{
   }
 
   private fetchLocationDetails($event: string) {
-    console.log("From Project Details Upper Section")
     let url = 'https://deckinspectors-dev.azurewebsites.net/api/location/getLocationById';
     let data = {
       locationid:$event,
