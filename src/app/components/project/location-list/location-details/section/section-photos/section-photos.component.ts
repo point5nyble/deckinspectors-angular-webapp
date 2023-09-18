f6878d6dfc4e6d286db4049d63bb34fc4c41cc2a
Ravidas Gaikwad <ravidasmg@gmail.com>
2023-09-18 19:27:39 +0530

    Image Carousel added

--- src/app/components/project/location-list/location-details/section/section-photos/section-photos.component.ts
+++ src/app/components/project/location-list/location-details/section/section-photos/section-photos.component.ts
@@ -8,8 +8,33 @@ import {InspectionReport} from "../../../../../../common/models/inspection-repor
 })
 export class SectionPhotosComponent implements OnInit{
   @Input() images!: string[];
+
+  previewImage = false;
+  showMask = false;
+  currentLightboxImage = '';
+  currentIndex = 0;
+
   ngOnInit(): void {
   }
 
+  openPreviewImage(index: number): void {
+    console.log("Index is"+index);
+    this.previewImage = true;
+    this.currentIndex = index;
+    this.currentLightboxImage = this.images[index];
+  }
 
+  closeImagePreview(): void {
+    this.previewImage = false;
+  }
+
+  prev(): void {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+    this.currentLightboxImage = this.images[this.currentIndex];
+  }
+
+  next(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    this.currentLightboxImage = this.images[this.currentIndex];
+  }
 }
