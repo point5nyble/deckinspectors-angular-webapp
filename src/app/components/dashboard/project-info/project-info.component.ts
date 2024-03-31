53fd85b0c8a8f9817e739738872035d88884ae5c
sudhanshu8352 <82777068+sudhanshu8352@users.noreply.github.com>
2024-03-31 03:54:11 -0300

    Home screen date fix

--- src/app/components/dashboard/project-info/project-info.component.ts
+++ src/app/components/dashboard/project-info/project-info.component.ts
@@ -1,9 +1,9 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
-import {AssignProjectModalComponent} from "../../../forms/assign-project-modal/assign-project-modal.component";
-import {UploadFilesModalComponent} from "../../../forms/upload-fiels-modal/upload-files-modal.component";
-import {DownloadFilesModalComponent} from "../../../forms/download-files-modal/download-files-modal.component";
-import {Project} from "../../../common/models/project";
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { AssignProjectModalComponent } from '../../../forms/assign-project-modal/assign-project-modal.component';
+import { UploadFilesModalComponent } from '../../../forms/upload-fiels-modal/upload-files-modal.component';
+import { DownloadFilesModalComponent } from '../../../forms/download-files-modal/download-files-modal.component';
+import { Project } from '../../../common/models/project';
 import { HttpsRequestService } from 'src/app/service/https-request.service';
 import { environment } from '../../../../environments/environment';
 import { DeleteConfirmationModalComponent } from '../../../forms/delete-confirmation-modal/delete-confirmation-modal.component';
@@ -11,46 +11,75 @@ import { DeleteConfirmationModalComponent } from '../../../forms/delete-confirma
 @Component({
   selector: 'app-project-info',
   templateUrl: './project-info.component.html',
-  styleUrls: ['./project-info.component.scss']
+  styleUrls: ['./project-info.component.scss'],
 })
 export class ProjectInfoComponent {
-  @Input() projectInfo!: Project;  
+  @Input() projectInfo!: Project;
   formattedDate!: string;
   @Output() projectAssignedEvent = new EventEmitter<any>();
   @Output() childClickEventTriggered = new EventEmitter<boolean>();
   @Output() markCompletedEvent = new EventEmitter<boolean>();
   @Output() projectEventDeletedEvent = new EventEmitter<any>();
   @Output() isDownloading = new EventEmitter<boolean>();
-  
-  constructor(private dialog: MatDialog, private httpsRequestService:HttpsRequestService) {}
 
-  ngOnInit(): void{
-      let createdDate= new Date(this.projectInfo.editedat);
-      let months: string[] = [
-      'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
-      'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
-  ];
-  let day: number =createdDate.getDate();
-  let monthIndex: number =createdDate.getMonth();
-  let year: number =createdDate.getFullYear();
-  this.formattedDate= `${months[monthIndex]} ${day}, ${year}`;
+  constructor(
+    private dialog: MatDialog,
+    private httpsRequestService: HttpsRequestService
+  ) {}
+
+  ngOnInit(): void {
+    // const [year, month, day] = this.projectInfo.editedat.split('-');
+    const dateParts = this.projectInfo.editedat.split(/[-T:.Z]/);
+    const year = dateParts[0];
+    const month = this.getMonthName(parseInt(dateParts[1], 10));
+    const day = dateParts[2];
+
+    // Construct the formatted date string
+    this.formattedDate = `${month} ${day}, ${year}`;
   }
-  isAdmin: boolean = ((JSON.parse(localStorage.getItem('user')!))?.role === "admin");
-  Status:String='false';
+  getMonthName(month: number): string {
+    // Array of month names
+    const monthNames = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
+
+    // Get the month name based on the month number
+    return monthNames[month - 1];
+  }
+  isAdmin: boolean =
+    JSON.parse(localStorage.getItem('user')!)?.role === 'admin';
+  Status: String = 'false';
   openAssignProjectModal() {
     this.childClickEventTriggered.emit(true);
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
-    dialogConfig.width = "400px";
-    dialogConfig.height = "600px";
+    dialogConfig.width = '400px';
+    dialogConfig.height = '600px';
     dialogConfig.data = {
-      project: this.projectInfo
+      project: this.projectInfo,
     };
-    const dialogRef = this.dialog.open(AssignProjectModalComponent, dialogConfig);
-    dialogRef.afterClosed().subscribe(data => {
-      this.projectAssignedEvent.emit({isAssigned: data.isAssigned, apiCalled: data.apiCalled});
-     })
+    const dialogRef = this.dialog.open(
+      AssignProjectModalComponent,
+      dialogConfig
+    );
+    dialogRef.afterClosed().subscribe((data) => {
+      this.projectAssignedEvent.emit({
+        isAssigned: data.isAssigned,
+        apiCalled: data.apiCalled,
+      });
+    });
   }
 
   openUploadFilesModal() {
@@ -58,18 +87,16 @@ export class ProjectInfoComponent {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
-    dialogConfig.width = "700px";
-    dialogConfig.height = "700px";
+    dialogConfig.width = '700px';
+    dialogConfig.height = '700px';
     dialogConfig.data = {
-      "projectInfo": this.projectInfo
+      projectInfo: this.projectInfo,
     };
     const dialogRef = this.dialog.open(UploadFilesModalComponent, dialogConfig);
-    dialogRef.afterClosed().subscribe(data => {
+    dialogRef.afterClosed().subscribe((data) => {
       //data -> array of files uploaded
-      data.forEach((file: File) => {
-
-      })
-    })
+      data.forEach((file: File) => {});
+    });
   }
 
   openDownloadReportModal() {
@@ -77,29 +104,39 @@ export class ProjectInfoComponent {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
-    dialogConfig.width = "600px";
-    dialogConfig.height = "500px";
+    dialogConfig.width = '600px';
+    dialogConfig.height = '500px';
     dialogConfig.data = {
       id: 1,
-      project: this.projectInfo
+      project: this.projectInfo,
     };
-    const dialogRef = this.dialog.open(DownloadFilesModalComponent, dialogConfig);
-    dialogRef.afterClosed().subscribe(data => {
+    const dialogRef = this.dialog.open(
+      DownloadFilesModalComponent,
+      dialogConfig
+    );
+    dialogRef.afterClosed().subscribe((data) => {
       this.isDownloading.emit(data?.isDownloading);
-    })
+    });
   }
 
-  markComplete = () =>{
+  markComplete = () => {
     this.childClickEventTriggered.emit(true);
-    this.httpsRequestService.postHttpData<any>(`${environment.apiURL}/project/${this.projectInfo._id}/toggleprojectstatus/${this.projectInfo.iscomplete ? 0 : 1}`, {}).subscribe(
-      (data) => {
-        this.markCompletedEvent.emit(!this.projectInfo.iscomplete);
-      },
-      error => {
-        console.log(error);
-      }
-    )
-  }
+    this.httpsRequestService
+      .postHttpData<any>(
+        `${environment.apiURL}/project/${
+          this.projectInfo._id
+        }/toggleprojectstatus/${this.projectInfo.iscomplete ? 0 : 1}`,
+        {}
+      )
+      .subscribe(
+        (data) => {
+          this.markCompletedEvent.emit(!this.projectInfo.iscomplete);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  };
 
   // moveProjectToFolder = () =>{
   //   this.childClickEventTriggered.emit(true);
@@ -109,29 +146,39 @@ export class ProjectInfoComponent {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
-    dialogConfig.width = "450px";
-    dialogConfig.height="230px"
-    dialogConfig.data={
-      name:this.projectInfo.name
-    }
+    dialogConfig.width = '450px';
+    dialogConfig.height = '230px';
+    dialogConfig.data = {
+      name: this.projectInfo.name,
+    };
     // dialogConfig.height = "140px";
-    const dialogRef = this.dialog.open(DeleteConfirmationModalComponent, dialogConfig);
-    dialogRef.afterClosed().subscribe(data => {
-      if(data.confirmed){
-        let url = environment.apiURL + "/project/" + this.projectInfo._id;
-        this.httpsRequestService.deleteHttpData<any>(url).subscribe(data => {
-          this.projectEventDeletedEvent.emit({project_id: this.projectInfo._id, state: true});
-        }, error => {
-          console.log(error);
-        })
-      }})
+    const dialogRef = this.dialog.open(
+      DeleteConfirmationModalComponent,
+      dialogConfig
+    );
+    dialogRef.afterClosed().subscribe((data) => {
+      if (data.confirmed) {
+        let url = environment.apiURL + '/project/' + this.projectInfo._id;
+        this.httpsRequestService.deleteHttpData<any>(url).subscribe(
+          (data) => {
+            this.projectEventDeletedEvent.emit({
+              project_id: this.projectInfo._id,
+              state: true,
+            });
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+      }
+    });
   }
-  StatusCheck(){
+  StatusCheck() {
     this.childClickEventTriggered.emit(true);
-    this.Status='true';
+    this.Status = 'true';
   }
 
-  folderOpen(){
+  folderOpen() {
     this.childClickEventTriggered.emit(true);
   }
 }
