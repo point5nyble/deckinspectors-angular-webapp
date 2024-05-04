83d81f5bc1596a56feb509e6e1cb5d6f67fc3378
sudhanshu8352 <82777068+sudhanshu8352@users.noreply.github.com>
2024-05-04 23:15:17 +0530

    complete filter fix

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { HttpsRequestService } from "../../service/https-request.service";
 import { Project } from "../../common/models/project";
 import { OrchestratorCommunicationService } from "../../orchestrator-service/orchestrartor-communication/orchestrator-communication.service";
@@ -10,23 +10,20 @@ import { ProjectQuery } from "../../app-state-service/project-state/project-sele
 import { LoginService } from "../login/login.service";
 import { Router } from "@angular/router";
 import { environment } from '../../../environments/environment';
-import {BackNavigation} from "../../app-state-service/back-navigation-state/back-navigation-selector";
+import { BackNavigation } from "../../app-state-service/back-navigation-state/back-navigation-selector";
 import { WebsocketConnectionService } from 'src/app/service/websocket-connection.service';
-
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-
-export class DashboardComponent implements OnInit
-{
+export class DashboardComponent implements OnInit {
   totalProjects: number = 0;
   completedProjects: number = 0;
   ongoingProjects: number = 0;
   showProjectInfo: string = 'home';
-  projectInfo! : Project;
+  projectInfo!: Project;
   projectInfos!: Project[];
   allProjects!: Project[];
   projectState!: ProjectState;
@@ -41,107 +38,77 @@ export class DashboardComponent implements OnInit
   downloadingReport: boolean = false;
   isFileUploaded: boolean = false;  //final report template
   isFileNotUploaded: boolean = false;  //final report template
-  notificationRecieved:boolean =false;
-  notificationMessage:string='';
-    constructor(private cdr: ChangeDetectorRef,
-                private httpsRequestService:HttpsRequestService,
-                private orchestratorCommunicationService:OrchestratorCommunicationService,
-                private store: Store<any>,
-                private loginService: LoginService,
-                private service: WebsocketConnectionService,
-                private router: Router) {}
+  notificationRecieved: boolean = false;
+  notificationMessage: string = '';
 
-    ngOnInit(): void {
-      this.performUserLoginSteps();
-      this.subscribeToshowProjectInfoToggle();
-      // To clear all exsting projects
-      this.gotoHome();
-      this.service.connect()
-                .subscribe((msg)=>{
-                  this.notificationMessage=msg.message;
-                  this.notificationRecieved=true;
-                })
-    }
-    
+  constructor(private cdr: ChangeDetectorRef,
+              private httpsRequestService: HttpsRequestService,
+              private orchestratorCommunicationService: OrchestratorCommunicationService,
+              private store: Store<any>,
+              private loginService: LoginService,
+              private service: WebsocketConnectionService,
+              private router: Router) { }
 
-  private fetchProjectData() {
+  ngOnInit(): void {
+    this.performUserLoginSteps();
+    this.subscribeToshowProjectInfoToggle();
+    // To clear all existing projects
+    this.gotoHome();
+    this.service.connect()
+      .subscribe((msg) => {
+        this.notificationMessage = msg.message;
+        this.notificationRecieved = true;
+      });
+  }
+
+  private fetchProjectData(onlyOngoing: boolean = true) {
     this.httpsRequestService.getHttpData<any>(`${environment.apiURL}/user/${localStorage.getItem('username')}`).subscribe(
       (user) => {
-        // console.log(user)
         localStorage.setItem('user', JSON.stringify(user));
-        if(user.role.toLowerCase() === "admin"){
+        if (user.role.toLowerCase() === "admin") {
           this.httpsRequestService.getHttpData<any>(`${environment.apiURL}/project/allProjects`).subscribe(
             (data) => {
-              this.projectInfos = this.filterProject(data.projects);
               this.allProjects = this.filterProject(data.projects);
+              if (onlyOngoing) {
+                this.projectInfos = this.filterProject(data.projects.filter((project: any) => !project.iscomplete));
+                // this.allProjects = this.filterProject(data.projects.filter((project: any) => !project.iscomplete));
+              } else {
+                this.projectInfos = this.filterProject(data.projects);
+              }
               this.calculateProjectStatistics();
             },
             error => {
               console.log(error);
             }
-          )
-        }
-        else {
-            this.httpsRequestService.getHttpData<any>(`${environment.apiURL}/project/getProjectsByUser/${localStorage.getItem('username')}`).subscribe(
-            (data) => {
-              this.projectInfos = this.filterProject(data.projects);
-              this.allProjects = this.filterProject(data.projects);
-            },
-            error => {
-              console.log(error);
-            }
-          )
-        }
-      },
-      error => {
-        console.log(error);
-      }
-    )
-  }
-
-  // This function is to get last element and called when we add new Project and automatically navigate to that function
-  private fetchProjectDataToGetLastElement() {
-    this.httpsRequestService.getHttpData<any>(`${environment.apiURL}/user/${localStorage.getItem('username')}`).subscribe(
-      (user) => {
-        if(user.role.toLowerCase() === "admin"){
-          this.httpsRequestService.getHttpData<any>(`${environment.apiURL}/project/allProjects`).subscribe(
-            (data) => {
-              this.projectInfos = this.filterProject(data.projects);
-              this.allProjects = this.filterProject(data.projects);
-              this.projectInfo = this.getRecentlyAddedProject(data.projects);
-              this.gotoProject(this.projectInfo);
-            },
-            error => {
-              console.log(error);
-            }
-          )
-        }
-        else {
+          );
+        } else {
           this.httpsRequestService.getHttpData<any>(`${environment.apiURL}/project/getProjectsByUser/${localStorage.getItem('username')}`).subscribe(
             (data) => {
-              this.projectInfos = this.filterProject(data.projects);
               this.allProjects = this.filterProject(data.projects);
-              this.projectInfo = this.getRecentlyAddedProject(data.projects);
-              this.gotoProject(this.projectInfo);
+              if (onlyOngoing) {
+                this.projectInfos = this.filterProject(data.projects.filter((project: any) => !project.iscomplete));
+                // this.allProjects = this.filterProject(data.projects.filter((project: any) => !project.iscomplete));
+              } else {
+                this.projectInfos = this.filterProject(data.projects);
+              }
             },
             error => {
               console.log(error);
             }
-          )
+          );
         }
       },
       error => {
         console.log(error);
       }
-    )
+    );
   }
 
   private calculateProjectStatistics() {
     this.totalProjects = this.allProjects.length;
-  
+
     const { completedCount, ongoingCount } = this.allProjects.reduce(
       (accumulator, project) => {
-        // console.log(project.iscomplete + " ");
         if (project.iscomplete === true || project.iscomplete === undefined) {
           accumulator.completedCount++;
         } else {
@@ -151,40 +118,35 @@ export class DashboardComponent implements OnInit
       },
       { completedCount: 0, ongoingCount: 0 }
     );
-  
+
     this.completedProjects = completedCount;
     this.ongoingProjects = ongoingCount;
   }
-  
-  
 
-  public gotoProject(projectInfo :Project): void {
-      if (this.isChildClickEventTriggered) {
-        this.isChildClickEventTriggered = false;
-          return;
-      }
+  public gotoProject(projectInfo: Project): void {
+    if (this.isChildClickEventTriggered) {
+      this.isChildClickEventTriggered = false;
+      return;
+    }
     this.projectInfo = projectInfo;
-    //TODO: Remove this temp solution
     projectInfo.type = 'project';
     this.orchestratorCommunicationService.publishEvent(OrchestratorEventName.Add_ELEMENT_TO_PREVIOUS_BUTTON_LOGIC,
       ObjectCloneServiceService.deepClone(projectInfo));
     if (projectInfo.projecttype === 'multilevel') {
-      this.orchestratorCommunicationService.publishEvent(OrchestratorEventName.SHOW_SCREEN,'project')
+      this.orchestratorCommunicationService.publishEvent(OrchestratorEventName.SHOW_SCREEN, 'project');
     } else {
-      this.orchestratorCommunicationService.publishEvent(OrchestratorEventName.SHOW_SCREEN,'location')
+      this.orchestratorCommunicationService.publishEvent(OrchestratorEventName.SHOW_SCREEN, 'location');
     }
-    // this.disableInvasiveBtn = !this.projectInfo.isInvasive;
   }
 
   private subscribeToshowProjectInfoToggle() {
     this.orchestratorCommunicationService.getSubscription(OrchestratorEventName.SHOW_SCREEN).subscribe(data => {
       this.showProjectInfo = data;
       if (data === 'home') {
-        // this.disableInvasiveBtn = false;
         this.fetchProjectData();
-        this.orchestratorCommunicationService.publishEvent(OrchestratorEventName.PROJECT_STATE_UPDATE, {state:ProjectState.VISUAL});
+        this.orchestratorCommunicationService.publishEvent(OrchestratorEventName.PROJECT_STATE_UPDATE, { state: ProjectState.VISUAL });
       }
-    })
+    });
   }
 
   projectSearch($event: string) {
@@ -194,12 +156,12 @@ export class DashboardComponent implements OnInit
   }
 
   newProjectUploaded() {
-      // add Timeout
-      setTimeout(() => {
-        this.fetchProjectDataToGetLastElement();
-      },1000)
+    setTimeout(() => {
+      this.fetchProjectData();
+    }, 1000);
   }
-  private filterProject(projects:Project[]): Project[] {
+
+  private filterProject(projects: Project[]): Project[] {
     if (this.projectState === ProjectState.INVASIVE) {
       return projects.filter(project => project.isInvasive);
     }
@@ -207,7 +169,7 @@ export class DashboardComponent implements OnInit
   }
 
   private getRecentlyAddedProject(projects: Project[]) {
-      return projects[0];
+    return projects[0];
   }
 
   childClickEventTriggered($event: boolean) {
@@ -218,125 +180,128 @@ export class DashboardComponent implements OnInit
 
   private performUserLoginSteps() {
     if (this.loginService.isLoggedIn()) {
-        this.fetchProjectData();
+      this.fetchProjectData();
     } else {
-      this.router.navigate(['/login'])
+      this.router.navigate(['/login']);
     }
   }
 
   gotoHome() {
-    let tempList:any[] = [];
+    let tempList: any[] = [];
     this.store.select(BackNavigation.getPreviousStateModelChain).subscribe(chain => {
       tempList = chain.stack;
-    })
-    tempList.forEach((element:any) => {
+    });
+    tempList.forEach((element: any) => {
       if (element.name !== 'Home') {
         this.orchestratorCommunicationService.publishEvent(OrchestratorEventName.REMOVE_ELEMENT_FROM_PREVIOUS_BUTTON_LOGIC, '');
       }
-    })
-    this.orchestratorCommunicationService.publishEvent(OrchestratorEventName.SHOW_SCREEN,'home');
+    });
+    this.orchestratorCommunicationService.publishEvent(OrchestratorEventName.SHOW_SCREEN, 'home');
   }
 
-  compare = (a:Project, b:Project) =>{
+  compare = (a: Project, b: Project) => {
     let x = a._id.toLowerCase();
     let y = b._id.toLowerCase();
-    if (x < y) {return 1;}
-    if (x > y) {return -1;}
+    if (x < y) { return 1; }
+    if (x > y) { return -1; }
     return 0;
   }
 
-  projectAssigned = (event: any) =>{
-
+  projectAssigned = (event: any) => {
     this.isProjectAssigned = event.isAssigned;
     this.apiCalled = event.apiCalled;
 
-    if (event.isAssigned && event.apiCalled){
+    if (event.isAssigned && event.apiCalled) {
       this.fetchProjectData();
       setTimeout(this.removeNotification, 5000);
     }
   }
 
-  filterCompleted = (isChecked: boolean) =>{
-    if(isChecked){
-    this.httpsRequestService.getHttpData<any>(`${environment.apiURL}/user/${localStorage.getItem('username')}`).subscribe(
-      (user) => {
-        localStorage.setItem('user', JSON.stringify(user));
-        if(user.role.toLowerCase() === "admin"){
-          this.httpsRequestService.getHttpData<any>(`${environment.apiURL}/project/allProjects`).subscribe(
-            (data) => {
-              this.projectInfos = data.projects.filter((project: any) => project.iscomplete).sort(this.compare);
-              this.allProjects = data.projects.filter((project: any) => project.iscomplete).sort(this.compare);
-            },
-            error => {
-              console.log(error);
-            }
-          )
-        }
-        else {
+  filterCompleted = (isChecked: boolean) => {
+    if (isChecked) {
+      this.httpsRequestService.getHttpData<any>(`${environment.apiURL}/user/${localStorage.getItem('username')}`).subscribe(
+        (user) => {
+          localStorage.setItem('user', JSON.stringify(user));
+          if (user.role.toLowerCase() === "admin") {
+            this.httpsRequestService.getHttpData<any>(`${environment.apiURL}/project/allProjects`).subscribe(
+              (data) => {
+                this.projectInfos = data.projects.filter((project: any) => project.iscomplete).sort(this.compare);
+                // this.allProjects = data.projects.filter((project: any) => project.iscomplete).sort(this.compare);
+              this.calculateProjectStatistics();
+
+              },
+              error => {
+                console.log(error);
+              }
+            );
+          }
+          else {
             this.httpsRequestService.getHttpData<any>(`${environment.apiURL}/project/getProjectsByUser/${localStorage.getItem('username')}`).subscribe(
-            (data) => {
-              this.projectInfos = data.projects.filter((project: any) => project.iscomplete).sort(this.compare);
-              this.allProjects = data.projects.filter((project: any) => project.iscomplete).sort(this.compare);
-            },
-            error => {
-              console.log(error);
-            }
-          )
+              (data) => {
+                this.projectInfos = data.projects.filter((project: any) => project.iscomplete).sort(this.compare);
+                // this.allProjects = data.projects.filter((project: any) => project.iscomplete).sort(this.compare);
+              this.calculateProjectStatistics();
+
+              },
+              error => {
+                console.log(error);
+              }
+            );
+          }
+        },
+        error => {
+          console.log(error);
         }
-      },
-      error => {
-        console.log(error);
-      }
-    )
+      );
 
+    }
+    else {
+      this.fetchProjectData();
+    }
   }
-  else{
-    this.fetchProjectData();
-  }
-}
 
-fileUploaded(isUploaded: boolean){
-  isUploaded? this.isFileUploaded = isUploaded : this.isFileNotUploaded = isUploaded;
-  setTimeout(this.removeNotification, 5000);
-}
-
-markedCompleted = (completed: boolean) =>{
-  if (completed){
-    this.fetchProjectData();
-    this.showProjectCompleteAlert = true;
-  } else{
-    this.filterCompleted(true);
-    this.showProjectInProgressAlert = true;
-  }
-  setTimeout(this.removeNotification, 5000);
-}
-
-removeNotification = () =>{
-  this.showProjectCompleteAlert = false;
-  this.showProjectInProgressAlert = false;
-  this.isProjectAssigned = false;
-  this.apiCalled = false;
-  this.isDeleteSuccess = false;
-  this.isDeleteFail = false;
-  this.isFileUploaded = false;
-  this.isFileNotUploaded = false;
-  this.downloadingReport = false;
-}
-
-  projectEventDeletedEvent($event: any) {
-    if($event.state){
-      setTimeout(() => {
-        this.fetchProjectData();
-        this.isDeleteSuccess = $event.state;
-      },1000)
-  }
-  else{
-    this.isDeleteFail = $event.state;
-  }
+  fileUploaded(isUploaded: boolean) {
+    isUploaded ? this.isFileUploaded = isUploaded : this.isFileNotUploaded = isUploaded;
     setTimeout(this.removeNotification, 5000);
   }
 
-  reportDownloadEvent($event: any){
+  markedCompleted = (completed: boolean) => {
+    if (completed) {
+      this.fetchProjectData();
+      this.showProjectCompleteAlert = true;
+    } else {
+      this.filterCompleted(true);
+      this.showProjectInProgressAlert = true;
+    }
+    setTimeout(this.removeNotification, 5000);
+  }
+
+  removeNotification = () => {
+    this.showProjectCompleteAlert = false;
+    this.showProjectInProgressAlert = false;
+    this.isProjectAssigned = false;
+    this.apiCalled = false;
+    this.isDeleteSuccess = false;
+    this.isDeleteFail = false;
+    this.isFileUploaded = false;
+    this.isFileNotUploaded = false;
+    this.downloadingReport = false;
+  }
+
+  projectEventDeletedEvent($event: any) {
+    if ($event.state) {
+      setTimeout(() => {
+        this.fetchProjectData();
+        this.isDeleteSuccess = $event.state;
+      }, 1000);
+    }
+    else {
+      this.isDeleteFail = $event.state;
+    }
+    setTimeout(this.removeNotification, 5000);
+  }
+
+  reportDownloadEvent($event: any) {
     this.downloadingReport = $event;
     setTimeout(this.removeNotification, 5000);
   }
