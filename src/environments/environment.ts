4081c0eef08f32d661e7c72f864a25bde9792c10
abhinovpankaj <abhinovpankaj1@hotmail.com>
2026-04-02 20:51:30 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     //apiURL: 'http://localhost:3000/api'
-    apiURL: 'https://deckmultitenantwebservices-uat.azurewebsites.net/api'
+    apiURL: 'https://deckmultitenantwebservices.azurewebsites.net/api'
     
 };
